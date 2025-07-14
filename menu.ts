a4a8047f81380f1fa18811c22650bce5f3c0d299
fullstackfeal <felipealves.santos93@gmail.com>
2025-07-14 01:22:15 -0300

    Conta Bancária

--- menu.ts
+++ menu.ts
@@ -1,20 +1,12 @@
 import readlinesync = require("readline-sync");
-import { colors } from "./src/util/colors";
+import { Colors } from "./src/util/colors";
 import { Conta } from "./src/model/conta";
-
 export function main() {
 
 let opcao: number;
 
-const conta: Conta = new Conta(1, 123, 1, "Adraiana", 10000);
-conta.visualizar();
-conta.sacar(10500);
-conta.visualizar();
-conta.depositar(5000);
-conta.visualizar();
-
 while (true) {
-    console.log(colors.bg.cyanbright, colors.fg.crimson,
+    console.log(Colors.bg.cyanbright, Colors.fg.crimson,
                 "_________________________________________________________________");
     console.log("                                                                   ");
     console.log("                               BANKEND                             ");
@@ -30,54 +22,54 @@ while (true) {
     console.log("               9 - Sair                                            ");
     console.log("___________________________________________________________________");
     console.log("                                                                  ",
-    colors.reset);
+    Colors.reset);
     console.log(" Entre com a opção desejada: ");
 
     opcao = readlinesync.questionInt("");
 
     if (opcao == 9) {
-        console.log(colors.fg.cyan, 
+        console.log(Colors.fg.cyan, 
             "\nBankEnd - Conta digital do jeitinho que você programa!");
         sobre();
-        console.log(colors.reset, "");
+        console.log(Colors.reset, "");
         process.exit(0);
     }
 
     switch (opcao) {
         case 1:
-            console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong, "\n\nCriar Conta\n\n", Colors.reset);
             keyPress()
             break;
         case 2:
-            console.log(colors.fg.whitestrong,"\n\nListar todas as Contas\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nListar todas as Contas\n\n", Colors.reset);
             keyPress()
             break;
         case 3:
-            console.log(colors.fg.whitestrong,"\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nConsultar dados da Conta - por número\n\n", Colors.reset);
             keyPress()
             break;
         case 4:
-            console.log(colors.fg.whitestrong,"\n\nAtualizar dados da Conta\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nAtualizar dados da Conta\n\n", Colors.reset);
             keyPress()
             break;
         case 5:
-            console.log(colors.fg.whitestrong,"\n\nApagar uma Conta\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nApagar uma Conta\n\n", Colors.reset);
             keyPress()
             break;
         case 6:
-            console.log(colors.fg.whitestrong,"\n\nSaque\n\n"), colors.reset;
+            console.log(Colors.fg.whitestrong,"\n\nSaque\n\n"), Colors.reset;
             keyPress()
             break;
         case 7:
-            console.log(colors.fg.whitestrong,"\n\nDepósito\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nDepósito\n\n", Colors.reset);
             keyPress()
             break;
         case 8:
-            console.log(colors.fg.whitestrong,"\n\nTransferência entre Contas\n\n", colors.reset);
+            console.log(Colors.fg.whitestrong,"\n\nTransferência entre Contas\n\n", Colors.reset);
             keyPress()
             break;
         default:
-            console.log(colors.fg.redstrong,"\nOpção Inválida!\n", colors.reset);
+            console.log(Colors.fg.redstrong,"\nOpção Inválida!\n", Colors.reset);
             keyPress()
             break;
     }
@@ -93,7 +85,7 @@ export function sobre(): void {
 }
 
 function keyPress(): void {
-    console.log(colors.reset, "");
+    console.log(Colors.reset, "");
     console.log("\nPressione enter para continuar...");
     readlinesync.prompt();
 }
